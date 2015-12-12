2681b25c8028236b332f41cedb49330b4fed7597
Arnav Gupta <championswimmer@gmail.com>
2015-12-12 06:58:35 +0000

    some progress

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-gmail = require('./modules/gmail-client')
+'use strict';
+var express = require("express");
+//var passport = require("passport");
+var gmail = require('./modules/gmail-client');
 
-gmail.fetchEmail("some_api_key");
+var app = express();
+
+gmail.setupLoginPages(app)
+  
+  
+
+//gmail.fetchEmail("some_api_key");
+
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = process.env.IP;
+  var port = process.env.PORT;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
--- modules/gmail-client/index.js
+++ modules/gmail-client/index.js
@@ -1,11 +1,49 @@
 'use strict';
+var secrets = require("./client_secrets.json")
+var passport = require("passport");
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./userDB');
+
+GoogleStrategy.prototype.userProfile = function(token, done) {
+  done(null, {})
+}
+
+passport.use(new GoogleStrategy({
+    clientID: secrets.web.client_id,
+    clientSecret: secrets.web.client_secret,
+    callbackURL: "https://offline-email-championswimmer.c9users.io/auth/google/callback",
+  },
+  function(accessToken, refreshToken, profile, done) {
+    localStorage.refreshToken = refreshToken;
+    localStorage.accessToken = accessToken;
+    console.log("refresh token = " + localStorage.refreshToken);
+    console.log("access token = " + localStorage.accessToken);
+  }
+));
+
 var GmailClient = {
     globalVar : "aaaa",
+}
+
+GmailClient.setupLoginPages = function(expressApp) {
+    expressApp.get('/auth/google',
+  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/gmail.modify' }));
+  
+  expressApp.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log(req);
+    // res.redirect('/');
+  });
+  
 }
 
 GmailClient.fetchEmail = function(fetchEmail) {
     console.log('fetching email ' + this.globalVar);
 }
 
+GmailClient.passport = passport;
 
 module.exports = GmailClient;
